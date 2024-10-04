2d23e461313b252c0a400bc7bc67e3b315f6280c
Globsest <globs.noone@mail.ru>
2024-10-04 09:36:47 +0300

    first commit

---
+++ src/main/java/dev/projectx/moviesX/MoviesXApplication.java
@@ -0,0 +1,14 @@
+package dev.projectx.moviesX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesXApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MoviesXApplication.class, args);
+	}
+
+}
